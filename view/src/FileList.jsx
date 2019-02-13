c0f72b62edf9781651723f9ee7fd2d102ffb1884
Adam Swiezowski <adam.swiezowski+git@gmail.com>
2019-02-13 18:36:40 +0100

    remove oboslete code

--- view/src/FileList.jsx
+++ view/src/FileList.jsx
@@ -5,21 +5,21 @@ class FileList extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { 
-            files :  []
+        this.state = {
+            files: []
         };
         this.getFilelist();
     }
 
     getFilelist = () => {
         axios.get('api/files').then((response) => {
-           this.setState({ files: response.data });
+            this.setState({ files: response.data });
         }).catch(function (error) {
-            console.log(error);
-         });
+            console.error(error);
+        });
     }
 
-    download = (event, filename) =>{
+    download = (event, filename) => {
         event.preventDefault();
         axios.get("api/file/" + filename).then((response) => {
             return response;
@@ -27,30 +27,31 @@ class FileList extends Component {
     }
 
 
-    render() { 
+    render() {
         return (
-        <div>
-             <h5 className="text-center">
-             Files
-            </h5>
-            <div className="list-group">
-            {this.state.files.map(element => {
-                    return(
-                    <a /*onClick={(event) =>this.download(event, element.name)}*/  href={"api/file/"+element.name} className="list-group-item list-group-item-action">{element.name} <br />
-                    <div className="row">
-                        <div className="col">
-                            <span className="text-muted text-right small mr-1">{element.size}B</span>
-                        </div>
-                        <div className="col text-right">
-                           <span className="text-muted text-right small ml-1">{element.mtime}</span>
-                        </div>
-                    </div>
-                    
-                    </a>
-                )})}
-            </div>
-        </div> );
+            <div>
+                <h5 className="text-center">
+                    Files
+                </h5>
+                <div className="list-group">
+                    {this.state.files.map(element => {
+                        return (
+                            <a href={"api/file/" + element.name} className="list-group-item list-group-item-action">{element.name} <br />
+                                <div className="row">
+                                    <div className="col">
+                                        <span className="text-muted text-right small mr-1">{element.size}B</span>
+                                    </div>
+                                    <div className="col text-right">
+                                        <span className="text-muted text-right small ml-1">{element.mtime}</span>
+                                    </div>
+                                </div>
+
+                            </a>
+                        )
+                    })}
+                </div>
+            </div>);
     }
 }
-  
+
 export default FileList;
