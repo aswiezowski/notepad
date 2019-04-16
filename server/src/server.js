48f10f18e18becb94bf9a534bea063c6443320c5
Adam Swiezowski <adam.swiezowski+git@gmail.com>
2019-04-16 19:57:54 +0200

    dokku fix

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const path = require('path');
+
 var lastlogModule = require('./lastlog');
 var filesModule = require('./files');
 var textsModule = require('./texts');
 
-var API_PREFIX = '/api';
-
+const API_PREFIX = '/api';
+const INDEX_PATH = path.resolve('../view/build/index.html');
 const app = express();
-app.use(bodyParser.json());
-app.use(API_PREFIX + '/file', express.static('upload/'));
-app.use('/', express.static('../view/build/'));
-
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+app.use(API_PREFIX + '/file', express.static('upload/'));
 app.use(API_PREFIX + '/lastlog', lastlogModule);
 app.use(API_PREFIX + '/files', filesModule);
 app.use(API_PREFIX + '/texts', textsModule);
+
+app.use('/static', express.static('../view/build/static'));
+app.get('*', function(req, res) {
+    res.sendFile(INDEX_PATH);
+});
